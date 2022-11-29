a6ada016f53fc8d9b93a4e1439a2ceb45ae0235c
deep <shahdeep1908@gmail.com>
2022-11-29 11:25:53 +0530

    Heroku configs added

--- todoApp/settings.py
+++ todoApp/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-deep-todo-app.herokuapp.com']
+ALLOWED_HOSTS = ['django-todo-app-demo.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
